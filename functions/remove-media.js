f1eacfa7e900c07babc5a32a7a9733c91f815948
Gus Varallo <gustavo@varallo.me>
2022-08-03 13:34:04 -0300

    chore: first commit

---
+++ functions/remove-media.js
@@ -0,0 +1,28 @@
+const path = Runtime.getFunctions()['response-header'].path
+const response = require(path).response()
+
+exports.handler = async function (context, event, callback) {
+  const client = context.getTwilioClient()
+
+  const { Media, ConversationSid, MessageSid } = event
+
+  if (Media) {
+    await client.conversations.v1
+      .conversations(ConversationSid)
+      .messages(MessageSid)
+      .remove()
+      .then(async () => {
+        await client.conversations.v1
+          .conversations(ConversationSid)
+          .messages.create({ body: 'Mensagem com anexo removida' })
+          .then(message => console.log(message.sid))
+      })
+      .then(() => callback(null, response))
+      .catch(err => {
+        console.log(err)
+        return callback(err)
+      })
+  }
+
+  return callback(null, response)
+}
